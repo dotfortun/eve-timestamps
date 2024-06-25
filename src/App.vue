41825e7b3dc1c11b538c6724306d3814ca97868a
Shane Bell <being.for.itself@gmail.com>
2024-06-25 09:56:59 -0400

    spent like 15 minutes fixing a not-bug.

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ import Time from "./components/Time.vue";
 import DiscordTS from "./components/DiscordTS.vue";
 import TimeSetter from "./components/TimeSetter.vue";
 
+import "dot-components/dist/style.css";
+import { Credits } from "dot-components";
+
 const timezones = ref([
   { title: "GMT", tz: "utc" },
   { title: "US Eastern / New York", tz: "America/New_York" },
@@ -26,7 +29,7 @@ const { t } = useUrlSearchParams("history");
 const counter = useInterval(1000);
 const time = ref(DateTime.now());
 const timeModel = defineModel();
-const diceroll = ref(Math.random());
+const diceroll = ref(Math.random() < 0.05);
 
 watch(counter, () => {
   if (!timeModel.value) {
@@ -60,50 +63,18 @@ if (t !== undefined) {
       <Time :title="zone.title" :time="time" :tz="zone.tz" :counter="counter" />
     </template>
   </div>
-  <footer>
-    <div>
-      <small>
-        <ul>
-          <li class="list-none">
-            Created by <a href="https://github.com/dotfortun">dotfortun.</a>
-          </li>
-          <li class="list-none">
-            <template v-if="diceroll < 0.05">
-              Yes, this is basically just Nakamura Labs' Time tool but with
-              Discord timestamps.
-            </template>
-            <template v-else>
-              Donations to Peter Dostoevsky in Eve Online will be turned into
-              lossmails.
-            </template>
-          </li>
-          <li class="list-none">
-            <a
-              href="https://www.eveonline.com/signup?invc=5d52b821-87c0-4c81-bd0a-2cd88e9001b6"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Try Eve Online
-            </a>
-            or
-            <a
-              href="https://www.buymeacoffee.com/shanebelldev"
-              target="_blank"
-              rel="noopener noreferrer"
-              >buy me a coffee</a
-            >
-          </li>
-        </ul>
-      </small>
-    </div>
-    <div>
-      <p>
-        <a href="https://github.com/dotfortun/eve-timestamps/issues">
-          Found a bug? Tell us here!
-        </a>
-      </p>
-    </div>
-  </footer>
+  <Credits>
+    <template v-slot:extra>
+      <span v-if="diceroll">
+        Yes, this is basically just Nakamura Labs' Time tool but with Discord
+        timestamps.
+      </span>
+      <span v-else>
+        Donations to Peter Dostoevsky in Eve Online will be turned into
+        lossmails.
+      </span>
+    </template>
+  </Credits>
 </template>
 
 <style scoped>
